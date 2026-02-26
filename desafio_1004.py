4da58f868d6d3a005626f4ee079da23b6cba652b
anabbeatriz173-spec <anabbeatriz173@gmail.com>
2026-02-26 09:01:16 -0300

    Implement product calculation and formatted output

--- desafio_1004.py
+++ desafio_1004.py
@@ -15,3 +15,13 @@ caso contrário seu programa apresentará a mensagem: "Presentation Error".
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1004
 
 # Escreva sua solução abaixo
+
+# Leitura dos valores de entrada
+A = int(input())
+B = int(input())
+
+# Cálculo do produto
+PROD = A * B
+
+# Saída formatada (com espaços e letra maiúscula)
+print(f"PROD = {PROD}")
